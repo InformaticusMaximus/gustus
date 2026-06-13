2fea067e013dc546a12d0b4675c8746c2b7ec386
InformaticusMaximus <supermisha2055@gmail.com>
2026-06-13 18:27:32 +0200

    admin, browsable api, auth for URProfile, proper view for restaurants

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (Restaurant, EPI, RExperience, Rating, URProfile)
 
-# Register your models here.
+@admin.register(Restaurant, EPI, RExperience, Rating)
+class SuperAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(URProfile)
+class URProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "restaurant", "alias", "note"]
+    list_filder = ["user", "restaurant"]
+    search_fields = ["user__name", "restaurant__name", "alias"]
